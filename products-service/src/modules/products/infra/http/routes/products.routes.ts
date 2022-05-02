00398318e5b14cf192100489df5fdb6f762f528e
bitols <bitols@gmail.com>
2022-05-02 11:47:55 -0300

    validação de dados de requisições

--- products-service/src/modules/products/infra/http/routes/products.routes.ts
+++ products-service/src/modules/products/infra/http/routes/products.routes.ts
@@ -1,20 +1,83 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
-
 const productsController = new ProductsController();
 
-productsRouter.get('/:id', productsController.getById);
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().hex().required(),
+    },
+  }),
+  productsController.getById,
+);
 
-productsRouter.get('/depot/:id', productsController.getAllByDepotId);
+productsRouter.get(
+  '/depot/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().hex().required(),
+    },
+  }),
+  productsController.getAllByDepotId,
+);
 
-productsRouter.get('/client/:id', productsController.getAllByClientId);
+productsRouter.get(
+  '/client/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().hex().required(),
+    },
+  }),
+  productsController.getAllByClientId,
+);
 
-productsRouter.post('/', productsController.create);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      height: Joi.number().precision(5).required(),
+      width: Joi.number().precision(5).required(),
+      lenght: Joi.number().precision(5).required(),
+      price: Joi.number().precision(2).required(),
+      clientId: Joi.string().hex().required(),
+      depotId: Joi.string().hex().required(),
+    },
+  }),
+  productsController.create,
+);
 
-productsRouter.put('/:id', productsController.update);
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      height: Joi.number().precision(5).required(),
+      width: Joi.number().precision(5).required(),
+      lenght: Joi.number().precision(5).required(),
+      price: Joi.number().precision(2).required(),
+      clientId: Joi.string().hex().required(),
+      depotId: Joi.string().hex().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().hex().required(),
+    },
+  }),
+  productsController.update,
+);
 
-productsRouter.delete('/:id', productsController.delete);
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().hex().required(),
+    },
+  }),
+  productsController.delete,
+);
 
 export default productsRouter;
