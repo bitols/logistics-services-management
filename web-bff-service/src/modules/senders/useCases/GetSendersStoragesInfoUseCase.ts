79257bb5728ffda829d38ef0c7caa5de1bb2f5f1
bitols <bitols@gmail.com>
2022-06-04 16:03:31 -0300

    correção de buscas de dados de storages

--- web-bff-service/src/modules/senders/useCases/GetSendersStoragesInfoUseCase.ts
+++ web-bff-service/src/modules/senders/useCases/GetSendersStoragesInfoUseCase.ts
@@ -53,58 +53,18 @@ export class GetSendersStoragesInfoUseCase
         phone: x.phone,
         address: x.address,
         indicators: {
-          capacity: x.capacity.toFixed(3),
-          stored: details?.volume.toFixed(3),
-          usage: details ? ((details.volume * 100) / x.capacity).toFixed(2) : 0,
-          products: details?.qtd,
-          value: details?.value.toFixed(2),
+          capacity: x.capacity,
+          stored: details ? Number(details.volume.toFixed(3)) : 0,
+          usage: details
+            ? Number(((details.volume * 100) / x.capacity).toFixed(2))
+            : 0,
+          products: details ? details.qtd : 0,
+          value: details ? Number(details.value.toFixed(2)) : 0,
         },
       };
     });
 
     const response = await Promise.all(promises);
     return response;
-    //   const products = await this.productsGateway.getAllBySender({
-    //     senderId: data.id,
-    //   });
-
-    //   if (!products) {
-    //     throw new AppErrors('Products not found');
-    //   }
-
-    //   const storages = await Promise.all(
-    //     products
-    //       .map(x => x.storageId)
-    //       .filter((x, i, a) => a.indexOf(x) == i)
-    //       .map(async x => this.storagesGateway.getById({ id: x })),
-    //   );
-
-    //   return storages.map(x => {
-    //     const details = products
-    //       .filter(y => y.storageId == x?.id)
-    //       .map(x => {
-    //         return {
-    //           volume: x.height * x.width * x.lenght,
-    //           value: x.price,
-    //         };
-    //       })
-    //       .reduce((pre, cur) => {
-    //         return {
-    //           volume: pre.volume + cur.volume,
-    //           value: pre.value + cur.value,
-    //         };
-    //       });
-
-    //     return {
-    //       id: x?.id,
-    //       name: x?.name,
-    //       email: x?.email,
-    //       phone: x?.phone,
-    //       address: x?.address,
-    //       storedVolume: (Math.round(details.volume * 100) / 100).toFixed(3),
-    //       storedValue: (Math.round(details.value * 100) / 100).toFixed(2),
-    //     };
-    //   });
-    // }
   }
 }
