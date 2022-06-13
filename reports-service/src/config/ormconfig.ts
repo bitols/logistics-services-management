53f68977101406866202477b77e11e73eb20cd14
bitols <bitols@gmail.com>
2022-06-13 09:25:53 -0300

    refatoracao typeorm reports-service

--- reports-service/src/config/ormconfig.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-export = {
-  type: 'mongodb',
-  url: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-  useNewUrlParser: true,
-  synchronize: true,
-  logging: true,
-  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-};
--- reports-service/src/modules/reports/infra/typeorm/repositories/StoragesCapacityRepository.ts
+++ reports-service/src/modules/reports/infra/typeorm/repositories/StoragesCapacityRepository.ts
@@ -1,14 +1,15 @@
 import { IStoragesCapacityRepository } from '@modules/reports/domain/repositories/IStoragesCapacityRepository';
 import { IStorageCapacity } from '@shared-types/reports/domain/models/entities/IStorageCapacity';
 import { IRegisterStoragesCapacityRequest } from '@shared-types/reports/domain/models/requests/IRegisterStoragesCapacityRequest';
-import { getRepository, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import StorageCapacity from '../entities/StorageCapacity';
+import { dataSource } from '@shared/infra/typeorm';
 
 export class StoragesCapacityRepository implements IStoragesCapacityRepository {
   private ormRepository: Repository<StorageCapacity>;
 
   constructor() {
-    this.ormRepository = getRepository(StorageCapacity);
+    this.ormRepository = dataSource.getRepository(StorageCapacity);
   }
 
   public async create(
@@ -35,13 +36,11 @@ export class StoragesCapacityRepository implements IStoragesCapacityRepository {
 
   public async getByStorageId(
     storage: string,
-  ): Promise<IStorageCapacity | undefined> {
+  ): Promise<IStorageCapacity | null | undefined> {
     console.log(`get storage capacity reports by storageId: ${storage}`);
 
-    const storageCapacity = await this.ormRepository.findOne({
-      where: {
-        storageId: { $eq: storage },
-      },
+    const storageCapacity = await this.ormRepository.findOneBy({
+      storageId: storage,
     });
 
     return storageCapacity;
@@ -50,10 +49,8 @@ export class StoragesCapacityRepository implements IStoragesCapacityRepository {
   public async getAllBySender(sender: string): Promise<IStorageCapacity[]> {
     console.log(`get all storages capacity reports by senderId: ${sender}`);
 
-    const storagesCapacity = await this.ormRepository.find({
-      where: {
-        senderId: { $eq: sender },
-      },
+    const storagesCapacity = await this.ormRepository.findBy({
+      senderId: sender,
     });
 
     return storagesCapacity;
