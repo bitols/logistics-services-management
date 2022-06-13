5c0f22d8db8b9f7348f4e4efbe26b6d57da45bd6
bitols <bitols@gmail.com>
2022-06-13 08:24:25 -0300

    refatoracao typeorm storages-service

--- storages-service/src/modules/storages/domain/repositories/IStoragesRepository.ts
+++ storages-service/src/modules/storages/domain/repositories/IStoragesRepository.ts
@@ -5,7 +5,7 @@ export interface IStoragesRepository {
   create(data: ICreateStoragesRequest): Promise<IStorage>;
   save(storage: IStorage): Promise<IStorage>;
   remove(storage: IStorage): Promise<void>;
-  getById(id: string): Promise<IStorage | undefined>;
+  getById(id: string): Promise<IStorage | null | undefined>;
   getAllBySupplier(supplier: string): Promise<IStorage[]>;
   getAllBySender(sender: string): Promise<IStorage[]>;
 }
