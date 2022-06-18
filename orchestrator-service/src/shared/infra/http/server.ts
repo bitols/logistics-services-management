e7cdb6598adc116e04a3399e891d9a43c3e56891
bitols <bitols@gmail.com>
2022-06-18 12:10:05 -0300

    orchestrator consumindo novo topico kafka

--- orchestrator-service/src/shared/infra/http/server.ts
+++ orchestrator-service/src/shared/infra/http/server.ts
@@ -2,21 +2,21 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import appApiConfig from '@config/appApiConfig';
 import { app } from './app';
-import { storageCapacityControl } from '@modules/reports/infra/handler/storageCapacityControl.handler';
 import kafkaConfig from '@config/kafkaConfig';
 import { KafkaQueue } from '../kafka/KafkaQueue';
-import 'express-async-errors';
-import '@shared/container';
-import { container } from 'tsyringe';
+import { storageLocation } from '@modules/storages/infra/handler/storageLocation.handler';
+import { storageCapacityControl } from '@modules/reports/infra/handler/storageCapacityControl.handler';
 
 const port = appApiConfig.port;
 
 app.listen(port, async () => {
   console.log(`Server started on port ${port}!`);
 
-  const queue = container.resolve(KafkaQueue);
+  const queue = new KafkaQueue();
+
   await queue.startConsumer(
     kafkaConfig.storageCapacityTopic,
     storageCapacityControl,
   );
+  await queue.startConsumer(kafkaConfig.storageLocationTopic, storageLocation);
 });
