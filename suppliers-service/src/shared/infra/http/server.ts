2e9152a395121dadadbe57d74492f59eabbb0d4f
bitols <bitols@gmail.com>
2022-08-06 14:17:02 -0300

    refatoracao final suppliers-service

--- suppliers-service/src/shared/infra/http/server.ts
+++ suppliers-service/src/shared/infra/http/server.ts
@@ -1,15 +1,15 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import appConfig from '@config/appConfig';
+import appConfig from '@config/app/config';
 import { app } from './app';
-import { dataSource } from '../orm';
+import { dataSource } from '../../../config/orm';
 
 const port = appConfig.port;
 
 dataSource
   .initialize()
   .then(() => {
-    const server = app.listen(port, () => {
+    app.listen(port, () => {
       console.log(`Server started on port ${port}!`);
     });
   })
