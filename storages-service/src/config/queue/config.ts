d9fa52f301d9dcd16953b1141407fa8eb1974f15
bitols <bitols@gmail.com>
2022-10-14 08:17:44 -0300

    storages-service: alteração de produtor tópico produtos

--- storages-service/src/config/queue/config.ts
+++ storages-service/src/config/queue/config.ts
@@ -4,4 +4,5 @@ export default {
   clientId: process.env.KAFKA_CLIENT_ID as string,
   storageLocationTopic: process.env.STORAGE_LOCATION_TOPIC as string,
   storageCapacityTopic: process.env.STORAGE_CAPACITY_TOPIC as string,
+  storageProductTopic: process.env.STORAGE_PRODUCT_TOPIC as string,
 };
